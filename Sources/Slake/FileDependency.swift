f1b12d6f795b1600595d46d09bc74c090c460c4a
Max Desiatov <max@desiatov.com>
2020-11-28 23:03:44 +0000

    Prototype C executable query and dependencies

--- Sources/Slake/FileDependency.swift
+++ Sources/Slake/FileDependency.swift
@@ -9,9 +9,43 @@ import TSCBasic
 private let fsQueue = DispatchQueue.global(qos: .utility)
 
 struct FileDependency: Query {
-  let pathPatterns: [String]
+  let files: [AbsolutePath]
 
   func task(for runner: TaskRunner) -> Task<AbsolutePath, Error> {
     Empty().eraseToTask()
+  }
+}
+
+struct CObjectFile: Query {
+  let sourceFile: AbsolutePath
+
+  func task(for runner: TaskRunner) -> Task<AbsolutePath, Error> {
+    Empty().eraseToTask()
+  }
+}
+
+struct LinkExecutable: Query {
+  let objectFiles: [AbsolutePath]
+  let executableFile: AbsolutePath
+
+  func task(for runner: TaskRunner) -> Task<AbsolutePath, Error> {
+    Empty().eraseToTask()
+  }
+}
+
+struct BuildCExecutable: Query {
+  let sourceFiles: [AbsolutePath]
+  let executableFile: AbsolutePath
+  let parallelJobs = 1
+
+  func task(for runner: TaskRunner) -> Task<AbsolutePath, Error> {
+    runner(FileDependency(files: sourceFiles))
+      .flatMap(maxPublishers: .max(parallelJobs)) {
+        runner(CObjectFile(sourceFile: $0))
+      }
+      .collect()
+      .flatMap {
+        runner(LinkExecutable(objectFiles: $0, executableFile: executableFile))
+      }.eraseToTask()
   }
 }
